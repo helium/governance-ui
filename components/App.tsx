cd63467791d10c20e0178a857d62f4ac360a843a
bry <bryanzettler@gmail.com>
2024-03-12 17:03:44 -0500

    Add sitewide overlay

--- components/App.tsx
+++ components/App.tsx
@@ -40,6 +40,8 @@ import {
 } from '@sqds/iframe-adapter'
 import { WALLET_PROVIDERS } from '@utils/wallet-adapters'
 import { tryParsePublicKey } from '@tools/core/pubkey'
+import Button from './Button'
+import Link from 'next/link'
 
 const Notifications = dynamic(() => import('../components/Notification'), {
   ssr: false,
@@ -286,6 +288,29 @@ export function AppContents(props: Props) {
       <ErrorBoundary>
         <ThemeProvider defaultTheme="Dark">
           <WalletIdentityProvider appName={'Realms'}>
+            <div className="flex flex-col h-full w-full justify-center items-center backdrop-blur-md fixed inset-0 z-50 overflow-none">
+              <div className="flex flex-col p-6 rounded-md gap-8 max-w-xl bg-black/40">
+                <div className="flex flex-col flex-1">
+                  <h1>Helium Vote Has Moved</h1>
+                  <p className="text-lg">
+                    Important Update for the Helium Community
+                  </p>
+                </div>
+                <p className="max-w-md">
+                  The Helium Vote platform, where our community comes together
+                  to shape the future of the Network through decision-making,
+                  has transitioned to a new domain.
+                </p>
+                <p className="max-w-md">
+                  This move allows us to dedicate more resources and focus to
+                  enhance your voting experience, ensuring that every voice is
+                  heard more clearly and efficiently.
+                </p>
+                <Link href="https://heliumvote.com/">
+                  <Button>Join Us at the New Helium Vote</Button>
+                </Link>
+              </div>
+            </div>
             <GatewayProvider>
               <NavBar />
               <Notifications />
